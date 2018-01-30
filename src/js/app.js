00e622a100a1d767ff8ba05fcf3e6164407bde4b
Karel Nguyen <eparis@atlassian.com>
2018-01-30 02:43:37 +0100

    update interface

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,34 @@ particlesJS.load('particles-js', '/src/js/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
+//check if element has class
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+// hamburger click
 function hambClick() {
-  const hambBtn = document.getElementById('hamb-btn');
+  let hambBtn = document.getElementById('hamb-btn');
+  let menu = document.getElementById('menu');
+
   hambBtn.classList.toggle("is-active");
+
+  if (hasClass(hambBtn, 'is-active') === true) {
+    menu.style.display = 'block';
+  } else {
+    menu.style.display = 'none';
+  }
 };
 
+function closeMenu() {
+  let hambBtn = document.getElementById('hamb-btn');
+  let menu = document.getElementById('menu');
+  menu.style.display = 'none';
+  hambBtn.classList.toggle("is-active");
+}
+
 window.onscroll = function() {
-  const navbar = document.getElementById('navbar');
+  const navbar = document.getElementById('navbar-bc');
   const scrollNum = window.pageYOffset;
 
   if (scrollNum < 30) {
@@ -23,8 +44,8 @@ window.onscroll = function() {
 
 //TypeIt library for text type animation
 new TypeIt('#hello', {
-     strings: ['hello','hallo','你好','ahoj','salut','hola','ciao','привет','もしもし'],
-     speed: 150,
+     strings: ['hello','hallo','ahoj','salut','hola','你好','ciao','привет'],
+     speed: 250,
      breakLines: false,
      autoStart: false,
      cursor: true,
@@ -38,3 +59,6 @@ sr.reveal('.c-text2-wrapper');
 sr.reveal('.about-title');
 sr.reveal('.about-p');
 sr.reveal('.s-title');
+sr.reveal('.row');
+sr.reveal('.footer-content');
+sr.reveal('#menu');
